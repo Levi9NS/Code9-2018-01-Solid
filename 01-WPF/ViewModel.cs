bfa1c49c05016baa8fbca2913f6df7224e93de76
Milan Stanacev <milan.levi9@outlook.com>
2018-05-07 11:50:27 +0200

    wpf intro

---
+++ 01-WPF/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WpfApp
+{
+    public class ViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _clickCount;
+        private ICommand _clickMeCommand;
+
+        public ViewModel()
+        {
+            _clickMeCommand = new Command(() => ClickCount++);
+        }
+        
+        public int ClickCount
+        {
+            get { return _clickCount; }
+            set
+            {
+                // optional if value not changed
+                _clickCount = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(ClickCount)));
+            }
+        }
+
+        public ICommand ClickMeCommand => _clickMeCommand;
+
+    }
+}
